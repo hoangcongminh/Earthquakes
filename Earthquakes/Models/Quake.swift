cd4a803cbb897659bae15ee484d4090fd6d9df16
admin <congminh2320@gmail.com>
2023-09-13 15:54:17 +0700

    first commit

---
+++ Earthquakes/Models/Quake.swift
@@ -0,0 +1,30 @@
+/*
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A structure for representing quake data.
+ */
+
+import Foundation
+
+struct Quake {
+    let magnitude: Double
+    let place: String
+    let time: Date
+    let code: String
+    let detail: URL
+}
+
+extension Quake: Identifiable {
+    var id: String { code }
+}
+
+extension Quake: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case magnitude = "mag"
+        case place
+        case time
+        case code
+        case detail
+    }
+}
